53fbf4b7c9984226fd55c26053a26a67478a8f36
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2020-01-22 23:03:23 +0500

    first commit

---
+++ src/main/java/ru/dkalugin/ImperiyaMetizov/ImperiyaMetizovApplication.java
@@ -0,0 +1,13 @@
+package ru.dkalugin.ImperiyaMetizov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiyaMetizovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiyaMetizovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dkalugin/ImperiyaMetizov/ImperiyaMetizovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dkalugin.ImperiyaMetizov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperiyaMetizovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
