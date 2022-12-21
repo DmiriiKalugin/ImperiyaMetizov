69838803221e8a298f196cc3c815b61cc4d66e46
DmiriiKalugin <40687176+DmiriiKalugin@users.noreply.github.com>
2022-12-21 21:37:33 +0500

    Update PropertiesMail.java

--- src/main/java/ru/dkalugin/ImperiyaMetizov/config/PropertiesMail.java
+++ src/main/java/ru/dkalugin/ImperiyaMetizov/config/PropertiesMail.java
@@ -1,9 +1,9 @@
 package ru.dkalugin.ImperiyaMetizov.config;
 
 public class PropertiesMail {
-    public static final String MY_EMAIL = "info@imperiya-metizov.ru";
-    public static final String MY_PASSWORD = "CfVcEyU1";
-    public static final String FRIEND_EMAIL = "sales@imperiya-metizov.ru";
+    public static final String MY_EMAIL = "info@mail.ru";
+    public static final String MY_PASSWORD = "root";
+    public static final String FRIEND_EMAIL = "sales@mail.ru";
     public static final String SUBJECT = "Заявка с сайта imperiya-metizov.ru";
     public static final String CART_SUBJECT = "Заказ с корзины сайта imperiya-metizov.ru";
 }
